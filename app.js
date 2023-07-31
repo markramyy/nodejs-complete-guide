f2e1b97c97b29fd32a5377d154e8be5569bc8141
markramyy <markramyy@gmail.com>
2023-07-31 12:26:22 +0300

    Parsing request Bodies

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ const server = http.createServer((req, res) => {
     }
     if (url === '/message' && method === 'POST')
     {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            // Buffer is a global variable
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         fs.writeFileSync('message.txt', 'DUMMY');
         res.statusCode = 302;
         res.setHeader('Location', '/');
